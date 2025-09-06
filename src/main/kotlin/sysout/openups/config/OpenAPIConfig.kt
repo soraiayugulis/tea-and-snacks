3c3f42039a12576498dcd242db52141ce9623015
soraia.yugulis <sys.yugulis@gmail.com>
2025-09-06 18:14:06 -0300

    add: enhance Swagger UI configuration and add OpenAPI definition
    Signed-off-by: soraia.yugulis <sys.yugulis@gmail.com>

---
+++ src/main/kotlin/sysout/openups/config/OpenAPIConfig.kt
@@ -0,0 +1,25 @@
+package sysout.openups.config
+
+import io.quarkus.arc.Unremovable
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.ws.rs.core.Application
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition
+import org.eclipse.microprofile.openapi.annotations.info.Info
+import org.eclipse.microprofile.openapi.annotations.tags.Tag
+
+@ApplicationScoped
+@Unremovable
+@OpenAPIDefinition(
+    info = Info(
+        title = "Tea 'n Snacks API",
+        version = "1.0.0"
+    ),
+    tags = [
+        Tag(name = "Teas", description = "Tea operations"),
+        Tag(name = "Snacks", description = "Snack operations"),
+        Tag(name = "Sauces", description = "Sauce operations")
+    ]
+)
+class OpenAPIConfig : Application() {
+    // A ordem das tags definidas acima determina a ordem de exibição no Swagger UI
+}
