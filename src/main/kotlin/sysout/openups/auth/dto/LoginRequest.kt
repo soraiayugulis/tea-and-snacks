ddefbe3bb24f49f53605ed5992f043f9ba3c4049
sysout <sys.yugulis@gmail.com>
2026-05-18 23:02:38 -0300

    fix(rbac): add Jackson Kotlin module for proper JSON serialization

--- src/main/kotlin/sysout/openups/auth/dto/LoginRequest.kt
+++ src/main/kotlin/sysout/openups/auth/dto/LoginRequest.kt
@@ -1,7 +1,11 @@
 package sysout.openups.auth.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class LoginRequest(
+    @JsonProperty("username")
     val username: String,
+    @JsonProperty("password")
     val password: String
 )
 
--- src/main/kotlin/sysout/openups/auth/dto/UpdateUserRolesRequest.kt
+++ src/main/kotlin/sysout/openups/auth/dto/UpdateUserRolesRequest.kt
@@ -1,12 +1,13 @@
 package sysout.openups.auth.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotEmpty
-import jakarta.validation.constraints.NotNull
 
 /**
  * Request DTO for updating user roles.
  */
 data class UpdateUserRolesRequest(
     @field:NotEmpty(message = "Roles cannot be empty")
+    @JsonProperty("roles")
     val roles: Set<String>
 )
